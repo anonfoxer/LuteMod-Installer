595fb8dfd4e1e5aa8e27341f2172b2c6705d1625
Magic <kevinandlife@gmail.com>
2021-09-18 21:48:02 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace LuteModInstaller
+{
+    partial class luteModInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(luteModInstaller));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathDisplayBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pathHelpButton = new System.Windows.Forms.Button();
+            this.defaultPathCheck = new System.Windows.Forms.CheckBox();
+            this.pathBrowseButton = new System.Windows.Forms.Button();
+            this.installButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.simpleInstallCheck = new System.Windows.Forms.CheckBox();
+            this.installOptsHelp = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Mordhau Path";
+            // 
+            // pathDisplayBox
+            // 
+            this.pathDisplayBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.pathDisplayBox.Location = new System.Drawing.Point(10, 39);
+            this.pathDisplayBox.Name = "pathDisplayBox";
+            this.pathDisplayBox.ReadOnly = true;
+            this.pathDisplayBox.Size = new System.Drawing.Size(337, 20);
+            this.pathDisplayBox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pathHelpButton);
+            this.groupBox1.Controls.Add(this.defaultPathCheck);
+            this.groupBox1.Controls.Add(this.pathBrowseButton);
+            this.groupBox1.Controls.Add(this.pathDisplayBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 124);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Path settings";
+            // 
+            // pathHelpButton
+            // 
+            this.pathHelpButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.pathHelpButton.Location = new System.Drawing.Point(97, 75);
+            this.pathHelpButton.Name = "pathHelpButton";
+            this.pathHelpButton.Size = new System.Drawing.Size(75, 39);
+            this.pathHelpButton.TabIndex = 4;
+            this.pathHelpButton.Text = "Help";
+            this.pathHelpButton.UseVisualStyleBackColor = true;
+            this.pathHelpButton.Click += new System.EventHandler(this.pathHelpButton_Click);
+            // 
+            // defaultPathCheck
+            // 
+            this.defaultPathCheck.AutoSize = true;
+            this.defaultPathCheck.Location = new System.Drawing.Point(198, 97);
+            this.defaultPathCheck.Name = "defaultPathCheck";
+            this.defaultPathCheck.Size = new System.Drawing.Size(149, 17);
+            this.defaultPathCheck.TabIndex = 3;
+            this.defaultPathCheck.Text = "Use default Mordhau path";
+            this.defaultPathCheck.UseVisualStyleBackColor = true;
+            this.defaultPathCheck.CheckedChanged += new System.EventHandler(this.defaultPathCheck_CheckedChanged);
+            // 
+            // pathBrowseButton
+            // 
+            this.pathBrowseButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.pathBrowseButton.Location = new System.Drawing.Point(10, 75);
+            this.pathBrowseButton.Name = "pathBrowseButton";
+            this.pathBrowseButton.Size = new System.Drawing.Size(75, 39);
+            this.pathBrowseButton.TabIndex = 2;
+            this.pathBrowseButton.Text = "Browse";
+            this.pathBrowseButton.UseVisualStyleBackColor = true;
+            this.pathBrowseButton.Click += new System.EventHandler(this.pathBrowseButton_Click);
+            // 
+            // installButton
+            // 
+            this.installButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.installButton.Location = new System.Drawing.Point(12, 203);
+            this.installButton.Name = "installButton";
+            this.installButton.Size = new System.Drawing.Size(353, 94);
+            this.installButton.TabIndex = 4;
+            this.installButton.Text = "Install";
+            this.installButton.UseVisualStyleBackColor = true;
+            this.installButton.Click += new System.EventHandler(this.installButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.simpleInstallCheck);
+            this.groupBox2.Controls.Add(this.installOptsHelp);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(12, 142);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(353, 55);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Install options";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // simpleInstallCheck
+            // 
+            this.simpleInstallCheck.AutoSize = true;
+            this.simpleInstallCheck.Location = new System.Drawing.Point(10, 24);
+            this.simpleInstallCheck.Name = "simpleInstallCheck";
+            this.simpleInstallCheck.Size = new System.Drawing.Size(108, 17);
+            this.simpleInstallCheck.TabIndex = 5;
+            this.simpleInstallCheck.Text = "Ignore modloader";
+            this.simpleInstallCheck.UseVisualStyleBackColor = true;
+            // 
+            // installOptsHelp
+            // 
+            this.installOptsHelp.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.installOptsHelp.Location = new System.Drawing.Point(272, 19);
+            this.installOptsHelp.Name = "installOptsHelp";
+            this.installOptsHelp.Size = new System.Drawing.Size(75, 23);
+            this.installOptsHelp.TabIndex = 4;
+            this.installOptsHelp.Text = "Help";
+            this.installOptsHelp.UseVisualStyleBackColor = true;
+            this.installOptsHelp.Click += new System.EventHandler(this.installOptsHelp_Click_1);
+            // 
+            // luteModInstaller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(372, 307);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.installButton);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "luteModInstaller";
+            this.Text = "anonfoxer\'s LuteMod Installer";
+            this.Load += new System.EventHandler(this.luteModInstaller_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathDisplayBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button pathHelpButton;
+        private System.Windows.Forms.CheckBox defaultPathCheck;
+        private System.Windows.Forms.Button pathBrowseButton;
+        private System.Windows.Forms.Button installButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button installOptsHelp;
+        private System.Windows.Forms.CheckBox simpleInstallCheck;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//I am well aware a lot of this code is garbage but i don't care, it's a simple tool for a niche game for a mod with an even nicher audience.
+
+namespace LuteModInstaller
+{
+    public partial class luteModInstaller : Form
+    {
+
+        // setting a bunch of variables
+        static string currentDir = Directory.GetCurrentDirectory(); // used for assets folder 
+        static string assetsPath = currentDir + @"\..\assets"; // Contains the needed .pak files
+        static string defaultMordhauPaksPath = @"C:\Program Files\Steam\steamapps\common\Mordhau\Mordhau\Content\Paks"; //Default mordhau install location paks folder
+        static string engineIniPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\..\Local\Mordhau\Saved\Config\WindowsClient\Engine.ini"; //engine.ini location
+        static string gameIniPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\..\Local\Mordhau\Saved\Config\WindowsClient\Game.ini"; //game.ini location
+        static string engineIniContent = @"[/Script/EngineSettings.GameMapsSettings]
+GameDefaultMap=/Game/Mordhau/Maps/ClientModMap/ClientMod_MainMenu.ClientMod_MainMenu"; // jargon for engine.ini
+        static string gameIniContent = @"[/Game/Mordhau/Maps/ClientModMap/BP_ClientModLoaderActor.BP_ClientModLoaderActor_C]
+ClientMods=/Game/Mordhau/Maps/LuteMod/Client/BP_LuteModClientLoader.BP_LuteModClientLoader_C
+ModListWidgetStayTime=5.0"; //jargon for game.ini
+        static string custPath = ""; //placeholder for custom game install path.
+
+        public luteModInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void installButton_Click(object sender, EventArgs e)
+        {
+            #region simpleInstall
+            if (simpleInstallCheck.Checked) //Checking to see if simple method
+            {
+                if (defaultPathCheck.Checked) { //For C: drive locations - simple
+                    try
+                    {
+                        File.Copy(assetsPath + @"\..\LuteMod.pak", defaultMordhauPaksPath);
+
+
+                        if (File.Exists(defaultMordhauPaksPath + @"\..\LuteMod.pak")) //check that it installed correctly
+                        {
+                            MessageBox.Show("LuteMod successfully installed, have fun!");
+                        }
+                        else {
+                            MessageBox.Show("LuteMod failed to install.");
+                        }
+                    }
+                    catch( Exception ex )
+                    {
+                        MessageBox.Show("Error when trying to install: " + ex);
+                    }
+                }
+                else //For any custom locations - simple
+                {
+                    try
+                    {
+                        //Copy LuteMod.pak to custPath + Mordhau\Content\Paks
+                        File.Copy(assetsPath + @"\..\LuteMod.pak", custPath + @"\..\steamapps\common\Mordhau\Mordhau\Content\Paks");
+
+
+                        if (File.Exists(custPath + @"\..\steamapps\common\Mordhau\Mordhau\Content\Paks\LuteMod.pak")) //check that it installed correctly
+                        {
+                            MessageBox.Show("LuteMod successfully installed, have fun!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("LuteMod failed to install.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error when trying to install: " + ex);
+                    }
+                }
+            }
+            #endregion
+
+            #region fullInstall
+            else
+            {
+                if (defaultPathCheck.Checked) //for C: Drive Locations
+                {
+                    try
+                    {
+                        File.Copy(assetsPath + @"\..\LuteMod.pak", defaultMordhauPaksPath);
+                        File.Copy(assetsPath + @"\..\zz_clientmodloadingmap_425.pak", defaultMordhauPaksPath);
+                        if (!engineIniPath.Contains(engineIniContent))
+                        {
+                            File.AppendAllText(engineIniPath, engineIniContent); // Append modloader code to .ini files, these dont need a custom path.
+                            File.AppendAllText(gameIniPath, gameIniContent);
+                        }
+
+                        if (File.Exists(defaultMordhauPaksPath + @"\..\LuteMod.pak")) //check that it installed correctly
+                        {
+                            MessageBox.Show("LuteMod successfully installed, have fun!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("LuteMod failed to install.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error when trying to install: " + ex);
+                    }
+                }
+                else //For any custom locations
+                {
+                    try
+                    {
+                        if (custPath != "")
+                        {
+                            //Copy LuteMod.pak to custPath + Mordhau\Content\Paks
+                            File.Copy(assetsPath + @"\..\LuteMod.pak", custPath + @"\..\steamapps\common\Mordhau\Mordhau\Content\Paks");
+                            File.Copy(assetsPath + @"\..\zz_clientmodloadingmap_425.pak", custPath + @"\..\steamapps\common\Mordhau\Mordhau\Content\Paks");
+                            if (!engineIniPath.Contains(engineIniContent))
+                            {
+                                File.AppendAllText(engineIniPath, engineIniContent); // Append modloader code to .ini files, these dont need a custom path.
+                                File.AppendAllText(gameIniPath, gameIniContent);
+                            }
+
+                            if (File.Exists(custPath + @"\..\steamapps\common\Mordhau\Mordhau\Content\Paks\LuteMod.pak")) //check that it installed correctly
+                            {
+                                MessageBox.Show("LuteMod successfully installed, have fun!");
+                            }
+                            else
+                            {
+                                MessageBox.Show("LuteMod failed to install.");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please select a path.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error when trying to install: " + ex);
+                    }
+                }
+            }
+            #endregion
+        }
+
+        private void pathHelpButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("If you have Mordhau installed anywhere other than the default C: location, click Browse and select the folder that contains your 'steamapps' folder.");
+        }
+
+        private void installOptsHelp_Click(object sender, EventArgs e)
+        {
+            //Removed feature, ignore me.
+        }
+
+        private void pathBrowseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog pathbrowser = new FolderBrowserDialog();
+            if (pathbrowser.ShowDialog() == DialogResult.OK)
+            {
+                custPath = pathbrowser.SelectedPath;
+                pathDisplayBox.Text = pathbrowser.SelectedPath;
+            }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void installOptsHelp_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enabling simple install will ignore the client side modloader, but will require you to manually load LuteMod on startup by entering \"open lutemod\" in the console. Good if you want to be able to go without lutemod running, or don't want to use the modloader.");
+        }
+
+        private void luteModInstaller_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void defaultPathCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            pathDisplayBox.Text = defaultMordhauPaksPath;
+        }
+    }
+}
+
+//else if else if else if else if else if else if else if else if else if else if else if else if else if else if else if else if - yandere dev 20212232
